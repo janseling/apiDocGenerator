6d4e198f8ee5b85142b6a8d68c7cea4edc4b62dc
Max Jane <max@quan0.com>
2018-06-02 10:48:28 +0800

    增加测试

---
+++ test/test.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @api
+ * @name    测试接口
+ * @url     test/test.php
+ * @method  GET
+ * @desc    这是用来测试接口1的接口，会直接输出无意义的数据
+ * @param   无      String  [可选]  其实并需要参数
+ * @column  json    这是一个随机返回的无意义json
+ */
+function test () {
+    $json = genJson();
+    return json_encode($json);
+}
+echo test();
+
+function genJson ($level = 0) {
+    $num = rand(3, 10);
+    $json = array();
+    for ($i = 0; $i < $num; $i++) {
+        if (rand(1, 100) > (25 / ($level + 1))) {
+            $json[genStr(true)] = genStr();
+            continue;
+        }
+        $json[genStr(true)] = genJson($level + 1);
+    }
+    return $json;
+}
+
+function genStr ($isName = false) {
+    $src = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $num = rand(1, $isName ? 16 : 64);
+    $str = '';
+    for ($i = 0; $i < $num; $i++) {
+        $index = rand(0, strlen($src) - 1);
+        $str .= $src[$index];
+    }
+    return $str;
+}
